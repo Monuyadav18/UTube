665156f2028877a43d29273dc2f4362c3decc17a
Monu Yadav <154664881+Monuyadav18@users.noreply.github.com>
2026-01-16 03:42:40 +0530

    Update Sidebar.jsx

--- src/Components/Navbar/Sidebar/Sidebar.jsx
+++ src/Components/Navbar/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import './Sidebar.css'
 
-import home from '../../../assets/Home.png'
+import home from '../../../assets/home.png'
 import game_icon from '../../../assets/game_icon.png'
 import automobiles from '../../../assets/automobiles.png'
 import sports from '../../../assets/sports.png'
